4f448b12a92649915b8ff378fc5696e5f5d2d16b
AraMilagros <50997261+AraMilagros@users.noreply.github.com>
2024-11-19 12:04:05 -0300

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -31,47 +31,6 @@ const auth = getAuth(app);
 export {auth};
 
 /**
- * Inicio de sesion
- */
-
-import { signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
-
-var contador = 0;
-
-// Manejar el login
-//INICIO CON CORREO Y CONTRASEÑA
-const loginForm = document.getElementById('loginForm');
-loginForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    contador = contador + 1;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-
-    signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            // Usuario logueado exitosamente
-            // Una vez que se autentica el email y pass, se redirecciona al home
-            window.location.href='home.html';
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            console.log(errorCode);
-            if(errorCode === "auth/invalid-login-credentials"){
-                alert('Email o Contraseña no son correctos.');
-            }
-            if(contador == 2){
-                alert('Puede que haya creado su cuenta vinculándola con Google. De no ser así, puede resetear su contraseña.');
-            }
-            // aqui se limpian los input
-            const email = document.getElementById('email');
-            const password = document.getElementById('password');
-            email.value="";
-            password.value='';
-        });
-});
-
-
-/**
  * ***************************************************************************
  * 
  * CERTIFICADO DE BAUTISMO
